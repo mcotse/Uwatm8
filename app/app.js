8f748efa7a082cc175960473462496dfc30e1826
Tony <pewpew@v1020-wn-19-14.campus-dynamic.uwaterloo.ca>
2016-03-05 04:59:38 -0500

    added prof page
    user prof page

--- app/app.js
+++ app/app.js
@@ -69,6 +69,7 @@ angular
           }
         }
       })
+
       .state('register', {
         url: '/register',
         controller: 'AuthCtrl as authCtrl',
@@ -87,6 +88,20 @@ angular
         url: '/chat',
         controller: 'AuthCtrl as authCtrl',
         templateUrl: 'channels/index.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
+      })
+      .state('userprof', {
+        url: '/userprof',
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/profile.html',
         resolve: {
           requireNoAuth: function($state, Auth){
             return Auth.$requireAuth().then(function(auth){
