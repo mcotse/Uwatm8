7e8e40602877165814afcf925b50bff1b479ca88
Matthew Tse <mtse9779@gmail.com>
2016-03-04 23:42:02 -0500

    working app

--- app/channels/channels.controller.js
+++ app/channels/channels.controller.js
@@ -7,10 +7,13 @@ angular.module('angularfireSlackApp')
     channelsCtrl.users = Users.all;
     channelsCtrl.getDisplayName = Users.getDisplayName;
     channelsCtrl.getGravatar = Users.getGravatar;
-
+    Users.setOnline(profile.$id);
     channelsCtrl.logout = function(){
-      Auth.$unauth();
-      $state.go('home');
+      channelsCtrl.profile.online = null;
+      channelsCtrl.profile.$save().then(function(){
+        Auth.$unauth();
+        $state.go('home');
+      });
     };
 
     channelsCtrl.newChannel = {
